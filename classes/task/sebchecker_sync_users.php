f5c5251f780dd3ac8c812d2b6378263b552b5870
Yann Cuttaz <cuttazy@USILU-204924.usilu.net>
2026-05-06 17:25:09 +0200

    only students in cron

--- classes/task/sebchecker_sync_users.php
+++ classes/task/sebchecker_sync_users.php
@@ -48,17 +48,22 @@ class sebchecker_sync_users extends \core\task\scheduled_task {
 
         // Get all users ID enrolled in courses between the dates.
         $sql = "SELECT DISTINCT ue.userid
-                FROM {user_enrolments} ue
-                JOIN {enrol} e ON e.id = ue.enrolid
-                JOIN {course} c ON c.id = e.courseid
-                WHERE ue.status = :active
-                  AND c.startdate >= :mindate
-                  AND c.startdate <= :maxdate";
+            FROM {user_enrolments} ue
+            JOIN {enrol} e ON e.id = ue.enrolid
+            JOIN {course} c ON c.id = e.courseid
+            JOIN {context} ctx ON ctx.instanceid = c.id AND ctx.contextlevel = :contextcourse
+            JOIN {role_assignments} ra ON ra.userid = ue.userid AND ra.contextid = ctx.id
+            WHERE ue.status = :active
+            AND c.startdate >= :mindate
+            AND c.startdate <= :maxdate
+            AND ra.roleid = :roleid";
 
         $params = [
             'active' => ENROL_USER_ACTIVE,
             'mindate' => $min_timestamp,
-            'maxdate' => $max_timestamp
+            'maxdate' => $max_timestamp,
+            'contextcourse' => CONTEXT_COURSE,
+            'roleid' => 5
         ];
 
         $users = $DB->get_records_sql($sql, $params);
